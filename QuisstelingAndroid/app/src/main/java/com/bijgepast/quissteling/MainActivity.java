7d3af27707424facd330e427be7765a11bdf5cec
WesleyR <wesleyroks4@gmail.com>
2021-06-03 14:52:18 +0200

    Add usercheck to MainActivity

--- QuisstelingAndroid/app/src/main/java/com/bijgepast/quissteling/MainActivity.java
+++ QuisstelingAndroid/app/src/main/java/com/bijgepast/quissteling/MainActivity.java
@@ -1,16 +1,34 @@
 package com.bijgepast.quissteling;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.bijgepast.quissteling.HomeActivity.HomeActivity;
+import com.bijgepast.quissteling.signin.SignInActivity;
 
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private UserSetting userSetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.userSetting = new UserSetting(this);
+
+        if (userSetting.exists()){
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            Log.d(LOG_TAG, "Username found sending to HomeActivity");
+            startActivity(intent);
+            userSetting.remove();
+        } else{
+            Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
+            Log.d(LOG_TAG, "No Username found sending to SignInActivity");
+            startActivity(intent);
+        }
     }
 }
