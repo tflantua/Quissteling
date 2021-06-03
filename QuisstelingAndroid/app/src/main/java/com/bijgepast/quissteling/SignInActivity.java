3b8cd2349ab507bcbc70b36f73cba342379d827a
WesleyR <wesleyroks4@gmail.com>
2021-06-03 11:39:56 +0200

    Added SignInActivity TODO's

--- QuisstelingAndroid/app/src/main/java/com/bijgepast/quissteling/SignInActivity.java
+++ QuisstelingAndroid/app/src/main/java/com/bijgepast/quissteling/SignInActivity.java
@@ -13,8 +13,11 @@ public class SignInActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     public static final String EXTRA_MESSAGE = "usernameMessageToMainActivity";
     private EditText usernameInput;
+    //UserSetting userSetting;
 
+    //TODO UserSettings class maken voor het gebruik van SharedPreferences over de hele applicatie
     //TODO Gebruik maken van SharedPreferences om de Username op te slaan. (Zo kunnen we uiteindelijk checken of de gebruiker meteen door mag gaan naar de MainActivity)
+    //TODO onDestroy methode maken dat de username opslaat in de SharedPrefrences doormiddel van de UserSettings class
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,7 @@ public class SignInActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //TODO Shared preferences meegeven
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 String message = usernameInput.getText().toString();
                 intent.putExtra(EXTRA_MESSAGE, message);
