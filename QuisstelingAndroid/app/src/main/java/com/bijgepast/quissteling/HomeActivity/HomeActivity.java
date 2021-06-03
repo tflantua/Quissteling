161c8b1b6c27bb16a8f21677ec59e1801b87da81
Luca <Luca.1999@hotmail.com>
2021-06-03 10:11:18 +0200

    Homescherm toegevoegd met kaart en buttons

---
+++ QuisstelingAndroid/app/src/main/java/com/bijgepast/quissteling/HomeActivity/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.bijgepast.quissteling.HomeActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bijgepast.quissteling.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
